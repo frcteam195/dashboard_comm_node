8e29938a42be44d02a6cf467402d9268df00ec98
sulicat <suli.barakat@gmail.com>
2022-12-02 18:36:48 -0500

    first commit

---
+++ src/dashboard_comm_node/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import tf2_ros
+import rospy
+import socket
+import json
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 41234
+BUFFER_SIZE  = 1024
+clients = [];
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+
+sock = socket.socket( socket.AF_INET,
+                      socket.SOCK_DGRAM )
+
+sock.setblocking(0)
+sock.bind( (UDP_IP, UDP_PORT) )
+
+def send(msg):
+    for c in clients:
+        sock.sendto( json.dumps(msg).encode("utf-8"), c )
+
+def got_veh_state():
+    send( {"asd":1, "asaaa":"aaa"} )
+
+def ros_main(node_name):
+    rospy.init_node(node_name)
+    rate = rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
+        try:
+            bytesAddressPair = sock.recvfrom(BUFFER_SIZE)
+            message = bytesAddressPair[0]
+            address = bytesAddressPair[1]
+
+            if address not in clients:
+                print("New Client: " + str(address))
+                clients.append(address);
+        except:
+            pass
+
+        got_veh_state();
+
+        rate.sleep()
