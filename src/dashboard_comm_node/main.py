37b3ae0bef391617626c34610d7281baeaa6a42b
Tom <TSLourenco@WPI.edu>
2022-12-09 15:07:15 -0500

    Added robot status.

--- src/dashboard_comm_node/main.py
+++ src/dashboard_comm_node/main.py
@@ -13,31 +13,30 @@ from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy,
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 41234
-BUFFER_SIZE  = 1024
+BUFFER_SIZE = 1024
 clients = []
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 
-sock = socket.socket( socket.AF_INET,
-                      socket.SOCK_DGRAM )
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.setblocking(0)
-sock.bind( (UDP_IP, UDP_PORT) )
+sock.bind((UDP_IP, UDP_PORT))
+
 
 def send(msg):
     for c in clients:
-        sock.sendto( json.dumps(msg).encode("utf-8"), c )
+        sock.sendto(json.dumps(msg).encode("utf-8"), c)
+
 
 def send_dashboard_packet():
-    global hmi_updates
     global robot_status
-    
-    send({"robot_status": robot_status})
-    pass
+    send({"robot_status": robot_status.get_message()})
+
 
 def ros_func():
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(10)  # 10hz
 
     while not rospy.is_shutdown():
         try:
@@ -53,9 +52,11 @@ def ros_func():
 
         rate.sleep()
 
+
 def ros_main(node_name):
     rospy.init_node(node_name)
-    
+    register_for_robot_updates()
+
     t1 = Thread(target=ros_func)
     t1.start()
 
