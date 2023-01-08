7bbd6175c378a791a6e679ad05ba7d067cb76908
Tom <TSLourenco@WPI.edu>
2023-01-08 15:26:52 -0500

    Whatever I had before disaster struck.

--- src/dashboard_comm_node/main.py
+++ src/dashboard_comm_node/main.py
@@ -4,44 +4,69 @@ import tf2_ros
 import rospy
 import socket
 import json
+import yaml
 
-UDP_IP = "127.0.0.1"
+from threading import Thread
+
+from frc_robot_utilities_py_node.frc_robot_utilities_py import *
+from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode
+
+UDP_IP = "0.0.0.0"
 UDP_PORT = 41234
-BUFFER_SIZE  = 1024
-clients = [];
+BUFFER_SIZE = 1024
+clients = []
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 
-sock = socket.socket( socket.AF_INET,
-                      socket.SOCK_DGRAM )
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.setblocking(0)
-sock.bind( (UDP_IP, UDP_PORT) )
+sock.bind((UDP_IP, UDP_PORT))
+
 
 def send(msg):
     for c in clients:
-        sock.sendto( json.dumps(msg).encode("utf-8"), c )
+        sock.sendto(json.dumps(msg).encode("utf-8"), c)
 
-def got_veh_state():
-    send( {"asd":1, "asaaa":"aaa"} )
 
-def ros_main(node_name):
-    rospy.init_node(node_name)
-    rate = rospy.Rate(10) # 10hz
+def send_dashboard_packet():
+    global hmi_updates
+    global robot_status
+
+    robot_status_data = ""
+    if robot_status is not None:
+        robot_status_data = robot_status.get_message()
+
+    send({"robot_status": robot_status_data})
+
+
+def loop():
+    rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
+
         try:
-            bytesAddressPair = sock.recvfrom(BUFFER_SIZE)
-            message = bytesAddressPair[0]
-            address = bytesAddressPair[1]
+            message, address = sock.recvfrom(BUFFER_SIZE)
 
             if address not in clients:
-                print("New Client: " + str(address))
-                clients.append(address);
+                print("New Client: " + str(address))t
+                clients.append(address)
         except:
             pass
 
-        got_veh_state();
+        send_dashboard_packet()
 
         rate.sleep()
+
+
+def ros_main(node_name):
+    rospy.init_node(node_name)
+    register_for_robot_updates()
+
+    t1 = Thread(target=loop)
+    t1.start()
+
+    rospy.spin()
+
+    t1.join(5)
